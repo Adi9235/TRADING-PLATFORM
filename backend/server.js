b85851a8d509aeb2e2d6cd12cccb83a5a24a3dea
Sujit Memane <114643903+sujitmemane@users.noreply.github.com>
2025-04-05 17:15:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,12 @@ connectDB();
 
 app.use(
   cors({
-    origin: "*",
-  
+    origin: ["https://trading-platform-snowy.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, 
   })
 );
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
