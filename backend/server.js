161428a19f3df231ebc2294ce1817f00dd979504
Sujit Memane <114643903+sujitmemane@users.noreply.github.com>
2025-04-05 17:43:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ let smartAPI = new SmartAPI({
 connectDB();
 
 const corsOptions = {
-  origin: "https://trading-platform-snowy.vercel.app",
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
